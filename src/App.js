414fa752aea701fea335bfeeb202770fd8f6ce84
Susie <susie.kim@hotmail.com>
2019-02-06 19:03:04 -0500

    setting up stuff

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
-import Landing from './routes/Landing';
-import Home from './routes/Home';
+import Login from './components/Login'
+
+
+// Once the user has logged in, they should be able to see the most recent repositories (repos) that use has forked and their most recent pull requests.
+
+// Each of the forked repos should display the name of that repo as a link, and link to the base repo.
+
+// Each of the pull requests should display the title of the pull request as a link, and link to that pull request.
+
+// Each of the pull requests should have, in addition, a visual indicator for whether it is open, closed or merged. This could be colour coded, or just have the status in words next to it.
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { 
+      username: '',
+      repos: [],
+      pulls: [],
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`https://api.github.com/users/${this.state.username}/events`)
+      .then(res => res.json())
+        .then(event => {
+          const pulls = repos.filter(event => repo.type === "PullRequest" )
+          const forks = repos.filter(repo => repo.type === "ForkRequest")
+          this.setState({{ forks, pulls }})
+        })
+        
+  }
+
+ 
+
   render() {
     return (
-      <Router>
-        <Switch>
-            <Route path="/user" component={Home}/>
-            <Route exact path="/" component={Landing}/>
-        </Switch>
-      </Router>
+      <div className="App">
+        <Login handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
+        <div className="Repos">
+       
+        </div>
+    </div>
     );
   }
 }
--- src/routes/Home.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Home = () => (
-    <h1>Text</h1>
-)
-
-export default Home;
